e4fea1fccff65afccffa2119c35654cfbc61aece
proforty7 <proforty7@gmail.com>
2018-06-23 16:38:42 +0530

    Improved Login

--- app/src/main/java/com/example/prateek/smackchat/controller/LoginActivity.kt
+++ app/src/main/java/com/example/prateek/smackchat/controller/LoginActivity.kt
@@ -1,9 +1,12 @@
 package com.example.prateek.smackchat.controller
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import com.example.prateek.smack.services.AuthService
 import com.example.prateek.smackchat.R
 import kotlinx.android.synthetic.main.activity_login.*
@@ -13,26 +16,66 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        loginSpinner.visibility = View.INVISIBLE
     }
 
     fun loginLoginBtnClicked(view: View) {
+        hideKeyboard()
+        enableSpinner(true)
         val email = loginEmailTxt.text.toString()
         val password = loginPasswordTxt.text.toString()
 
-        AuthService.loginUser(this, email, password) {loginSuccess ->
-            if(loginSuccess) {
-                AuthService.findUserByEmail(this) {findSuccess ->
-                    if(findSuccess) {
-                        finish()
+        if(email.isNotEmpty() && password.isNotEmpty()) {
+            AuthService.loginUser(this, email, password) {loginSuccess ->
+                if(loginSuccess) {
+                    AuthService.findUserByEmail(this) {findSuccess ->
+                        if(findSuccess) {
+                            enableSpinner(false)
+                            finish()
+                        } else {
+                            errorToast()
+                        }
                     }
+                } else {
+                    errorToast()
                 }
             }
+        } else {
+            Toast.makeText(this, "Fill the details bish!", Toast.LENGTH_SHORT).show()
         }
+
+
     }
 
     fun loginRegisterBtnClicked(view: View) {
         val registerIntent = Intent(this, RegisterActivity::class.java)
         startActivity(registerIntent)
         finish()
+    }
+
+    fun errorToast() {
+        Toast.makeText(this, "Something went wrong, please try again.", Toast.LENGTH_SHORT).show()
+        enableSpinner(false)
+    }
+
+    fun enableSpinner(enable: Boolean) {
+        if(enable) {
+            loginSpinner.visibility = View.VISIBLE
+
+        } else {
+            loginSpinner.visibility = View.INVISIBLE
+
+        }
+
+        loginLoginBtn.isEnabled = !enable
+        loginRegisterBtn.isEnabled = !enable
+    }
+
+    fun hideKeyboard() {
+        val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        if(inputManager.isAcceptingText) {
+            inputManager.hideSoftInputFromWindow(currentFocus.windowToken, 0)
+        }
     }
 }
